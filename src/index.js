eee6c75bd16e097493b8d28755e14fdac8fb4db1
Pratik Kumar <pratikumar56@gmail.com>
2025-02-26 17:40:03 +0530

    made a minor change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-require('dotenv').config();
-const models = require('./models/associations');
+const sequelize = require('./src/db');
+const models = require('./src/models/associations');
+const initializeDb = require('./init');
 
 module.exports = {
+  sequelize,
   models,
+  initializeDb,
 };
